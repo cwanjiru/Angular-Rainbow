1e2561a2f3e499887970d7c62708a4ea77d55a52
Christine <teenahshyro@gmail.com>
2020-08-25 16:57:19 +0300

    Add HostBinding to link the elements to the properties and Hostlistener to listen to the events.

--- src/app/rainbow.directive.ts
+++ src/app/rainbow.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appRainbow]'
 })
 export class RainbowDirective {
 
+  possible_colors=[
+    'darksalmon', 'hotpink', 'lightskyblue', 'goldenrod', 'peachpuff',
+    'mediumspringgreen', 'cornflowerblue', 'blanchedalmond', 'lightslategrey'
+  ]
   constructor() { }
+  @HostBinding('style.color') color:string;
+  @HostBinding('style.border-class') borderColor:string;
+  @HostListener('keydown') newColor(){
+    const colorPick = Math.floor(Math.random()* this.possible_colors.length);
+    this.color = this.borderColor = this.possible_colors[colorPick];
+  }
+
 
 }
